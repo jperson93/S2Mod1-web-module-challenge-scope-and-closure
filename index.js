2277f26a820289f548cfc9c61fb59728780af98f
Jb Person <homeaccount@Jbs-MacBook-Air.local>
2023-01-02 13:37:26 -0500

    task 4 complete

--- index.js
+++ index.js
@@ -115,10 +115,15 @@ For example: invoking getInningScore(inning) might return this object:
   */
 
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(inningCB) {
+  return {
+    Home: inningCB(),
+    Away: inningCB()
+  }
 
 }
+
+console.log(getInningScore(inning)); 
 
 
 /* STRETCH: ⚾️⚾️⚾️ Task 5: scoreboard() ⚾️⚾️⚾️
