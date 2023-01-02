1318320a5cfc118cd481a727fa9338c0b045a115
Jb Person <homeaccount@Jbs-MacBook-Air.local>
2023-01-02 13:15:10 -0500

    task 2 done

--- index.js
+++ index.js
@@ -64,9 +64,11 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+  return Math.round(Math.random() * 2);  // 0 to 100
 }
+
+console.log(inning());
 
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
